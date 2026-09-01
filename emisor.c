f54c14d04b22dd6ec9488702a22398e6cd0131ce
ekials <sucasacaalice@gmail.com>
2026-09-01 13:41:43 +0000

    Add files via upload

--- emisor.c
+++ emisor.c
@@ -11,7 +11,8 @@
 #define MAXSIZE 128
 #define KEY 1234
 
-struct msgbuf {
+struct msgbuf 
+{
     long mtype;
     char mtext[MAXSIZE];
 };
@@ -20,7 +21,8 @@ static sigjmp_buf punto_salto;
 static int msqid;
 static int contador[4] = {0, 0, 0, 0};
 
-void enviar_mensaje(int tipo) {
+void enviar_mensaje(int tipo) 
+{
     struct msgbuf sbuf;
     sbuf.mtype = tipo;
     snprintf(sbuf.mtext, MAXSIZE, "Mensaje tipo %d - PID %d - repeticion %d", tipo, getpid(), contador[tipo]);
@@ -30,28 +32,33 @@ void enviar_mensaje(int tipo) {
         printf("[EMISOR] Mensaje tipo %d enviado a la cola: \"%s\"\n", tipo, sbuf.mtext);
 }
 
-void handler_sigint(int signum) {
+void handler_sigint(int signum) 
+{
     contador[1]++;
-    printf("\n[F1] SIGINT recibido (%d). Van %d veces. Saltando...\n", signum, contador[1]);
+    printf("\n[F1] SIGINT recibido (%d). Van %d veces. Saltando\n", signum, contador[1]);
     siglongjmp(punto_salto, 1);
 }
 
-void handler_sigusr1(int signum) {
+void handler_sigusr1(int signum) 
+{
     contador[2]++;
-    printf("\n[F2] SIGUSR1 recibido (%d). Van %d veces. Saltando...\n", signum, contador[2]);
+    printf("\n[F2] SIGUSR1 recibido (%d). Van %d veces. Saltando\n", signum, contador[2]);
     siglongjmp(punto_salto, 2);
 }
 
-void handler_sigusr2(int signum) {
+void handler_sigusr2(int signum) 
+{
     contador[3]++;
-    printf("\n[F3] SIGUSR2 recibido (%d). Van %d veces. Saltando...\n", signum, contador[3]);
+    printf("\n[F3] SIGUSR2 recibido (%d). Van %d veces. Saltando\n", signum, contador[3]);
     siglongjmp(punto_salto, 3);
 }
 
-int main() {
+int main() 
+{
     printf("PID de este proceso (emisor): %d\n", getpid());
 
-    if ((msqid = msgget(KEY, IPC_CREAT | 0666)) < 0) {
+    if ((msqid = msgget(KEY, IPC_CREAT | 0666)) < 0) 
+    {
         perror("msgget");
         exit(1);
     }
@@ -62,17 +69,21 @@ int main() {
 
     int origen = sigsetjmp(punto_salto, 1);
 
-    if (origen == 0) {
-        printf("Programa iniciado. Esperando senales...\n");
-        printf("  kill -SIGINT  %d  -> mensaje tipo 1\n", getpid());
-        printf("  kill -SIGUSR1 %d  -> mensaje tipo 2\n", getpid());
-        printf("  kill -SIGUSR2 %d  -> mensaje tipo 3\n\n", getpid());
-    } else {
+    if (origen == 0) 
+    {
+        printf("Esperando senales\n");
+        printf("  kill -SIGINT  %d  - mensaje tipo 1\n", getpid());
+        printf("  kill -SIGUSR1 %d  - mensaje tipo 2\n", getpid());
+        printf("  kill -SIGUSR2 %d  - mensaje tipo 3\n\n", getpid());
+    } 
+    else 
+    {
         enviar_mensaje(origen);
     }
 
-    while (1) {
-        printf("Emisor activo, esperando senales...\n");
+    while (1) 
+    {
+        printf("Emisor activo - esperando senales\n");
         sleep(2);
     }
 
--- receptor.c
+++ receptor.c
@@ -8,17 +8,20 @@
 #define MAXSIZE 128
 #define KEY 1234
 
-struct msgbuf {
+struct msgbuf 
+{
     long mtype;
     char mtext[MAXSIZE];
 };
 
-void die(char *s) {
+void die(char *s) 
+{
     perror(s);
     exit(1);
 }
 
-int main() {
+int main() 
+{
     int msqid;
     struct msgbuf rcvbuffer;
 
@@ -26,27 +29,26 @@ int main() {
         die("msgget");
 
     printf("Receptor listo. PID: %d\n", getpid());
-    printf("Esperando mensajes de la cola (key=1234)...\n\n");
+    printf("Esperando mensajess \n\n");
 
-    while (1) {
+    while (1) 
+    {
         if (msgrcv(msqid, &rcvbuffer, MAXSIZE, 0, 0) < 0)
             die("msgrcv");
 
-        printf("========================================\n");
         printf("Mensaje recibido - Tipo: %ld\n", rcvbuffer.mtype);
         printf("Contenido: %s\n", rcvbuffer.mtext);
 
         if (rcvbuffer.mtype == 1)
-            printf("-> Procesando TIPO 1 (SIGINT)\n");
+            printf("Procesando TIPO 1 (SIGINT)\n");
         else if (rcvbuffer.mtype == 2)
-            printf("-> Procesando TIPO 2 (SIGUSR1)\n");
+            printf("Procesando TIPO 2 (SIGUSR1)\n");
         else if (rcvbuffer.mtype == 3)
-            printf("-> Procesando TIPO 3 (SIGUSR2)\n");
+            printf("Procesando TIPO 3 (SIGUSR2)\n");
         else
-            printf("-> Tipo desconocido\n");
+            printf("Tipo desconocido\n");
 
-        printf("Esperando 5 segundos...\n");
-        printf("========================================\n\n");
+        printf("Esperando 5 segundos\n");
         sleep(5);
     }
 
